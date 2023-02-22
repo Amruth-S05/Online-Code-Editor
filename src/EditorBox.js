7cf2a4e2592beddac98168cb0066fcaa345b39fa
abhayjit07 <100589347+abhayjit07@users.noreply.github.com>
2023-02-23 01:29:36 +0530

    Update EditorBox.js
    removed run button from editor box

--- src/EditorBox.js
+++ src/EditorBox.js
@@ -4,19 +4,21 @@ import Editor from "@monaco-editor/react";
 const EditorBox = ({theme, fontSize, lang}) => {
 
     return (
-        <div>
-            <Editor
-                className="editor"
-                height="80vh" width="100%"
+        <div className="editor">
+            <Editor              
                 theme={theme}
                 options={{fontSize: fontSize}}
                 language={lang}
+                defaultValue="// Enter your code here"
                 //defaultLanguage="javascript"
             />
-            <button className='run-btn'>Run</button>
+            
+            
         </div>
     );
 };
 
 export default EditorBox;
+
+
 
