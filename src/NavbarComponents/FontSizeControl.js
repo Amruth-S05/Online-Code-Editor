73edc63251e1fe89a3aa132cbdbd6d3626e03c1d
abhayjit07 <100589347+abhayjit07@users.noreply.github.com>
2023-03-22 01:42:24 +0530

    Update FontSizeControl.js
    corrected previous mistake

--- src/NavbarComponents/FontSizeControl.js
+++ src/NavbarComponents/FontSizeControl.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import Select from "react-select";
 
-const ThemeDropdown = ({theme, setTheme}) => {
-    const themes = [
-        {value: "vs", label: "Light"},
-        {value: "vs-dark", label: "Dark"},
-    ]
+const FontSizeControl = ({ fontSize, setFontSize }) => {
 
     return (
         <div>
-            <Select
-                options={themes}
-                value={theme}
-                onChange={(e) => setTheme(e.value)}
-                isSearchable={false}        // disable typing abiltiy in search bar.
-                placeholder={theme} />
+            <input
+                type="number" value={fontSize}
+                min="12" max="24"
+                onChange={(e) => setFontSize(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.keyCode !== 38 && e.keyCode !== 40) {
+                        e.preventDefault();
+                    }
+                }}
+            />
         </div>
     );
 };
 
-export default ThemeDropdown;
+export default FontSizeControl;
