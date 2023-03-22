564542dfcb2a1499ffb9935f1361bb69c63a5d3c
Amruth S <amruth.s05@gmail.com>
2023-03-22 20:58:20 +0530

    Replaced curl with request api to send post requests

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const {exec} = require('child_process');
+const request = require("request");
 
 const app = express();
+
+const clientId = "1131d8202307718b7ca2aa5c837ad2eb";
+const clientSecret = "f345b7f43e809ae2ea75b27bc4e351b6555865470d4010c5d1d1189221d5e45b";
 
 app.use(cors());
 app.use(express.json());
@@ -23,31 +27,36 @@ app.get("/favicon.ico", (req, res) => res.send(204));
 app.post("/execute", (req, res) => {
     console.log(req.body);
     const post_data = {
-        "clientId": "1131d8202307718b7ca2aa5c837ad2eb",
-        "clientSecret": "f345b7f43e809ae2ea75b27bc4e351b6555865470d4010c5d1d1189221d5e45b",
-        "stdin": req.body.stdin,
-        "script": req.body.script,
-        "language": req.body.language,
-        "versionIndex": req.body.versionId,
+        clientId: clientId,
+        clientSecret: clientSecret,
+        stdin: req.body.stdin,
+        script: req.body.script,
+        language: req.body.language,
+        versionIndex: req.body.versionId,
     };
 
-    const curl_cmd_exe = `curl -d '${JSON.stringify(post_data)}' -H "Content-Type: application/json" -X "POST" https://api.jdoodle.com/v1/execute`;
-
-    exec(curl_cmd_exe, (error, stdout, stderr) => {
-        if (error){
-            console.log("Error:", error);
-            res.send(error);
-        }
-        else{
-            console.log(stdout);
-            res.send(stdout);
-        }
+    request({
+        url: "https://api.jdoodle.com/v1/execute",
+        method: "POST",
+        json: post_data
+    },(error, response, body) => {
+        console.log("Error: ", error);
+        console.log("Response: ", response);
+        console.log("Body: ", body);
+        res.send(body);
     });
 });
 
 app.get("/usage", (req, res) => {
-    const curl_cmd_usage = `curl -d '{"clientId": "1131d8202307718b7ca2aa5c837ad2eb", "clientSecret": "f345b7f43e809ae2ea75b27bc4e351b6555865470d4010c5d1d1189221d5e45b"}' -H "Content-Type: application/json" -X "POST" https://api.jdoodle.com/v1/credit-spent`;
-    exec(curl_cmd_usage, (error, stdout, stderr) => {
-        res.send(stdout);
+    request({
+        url: "https://api.jdoodle.com/v1/credit-spent",
+        method: "POST",
+        json: {
+            clientId: clientId,
+            clientSecret: clientSecret,
+        }
+    }, (error, request, body) => {
+        const remainingCredits = 200 - body.used;
+        res.send({used: body.used, remaining: remainingCredits})
     });
 });
